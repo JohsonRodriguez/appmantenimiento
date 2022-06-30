09028dc36504eb416a8a355ddc700b0b99ad24b6
Johson Rodriguez <sukerjc@gmail.com>
2022-06-29 19:50:35 -0500

    ok

--- app/src/main/java/com/example/appmantenimiento/MenuActivity.java
+++ app/src/main/java/com/example/appmantenimiento/MenuActivity.java
@@ -2,13 +2,20 @@ package com.example.appmantenimiento;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
 
+    Button btn_ingProd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_ingProd=findViewById(R.id.btn_IngProducto);
+        btn_ingProd.setOnClickListener(v->{
+            startActivity(new Intent(getApplicationContext(), ProductoActivity.class));
+        });
     }
 }
--- app/src/main/java/com/example/appmantenimiento/ProductoActivity.java
+++ app/src/main/java/com/example/appmantenimiento/ProductoActivity.java
@@ -2,13 +2,21 @@ package com.example.appmantenimiento;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ProductoActivity extends AppCompatActivity {
-
+     ImageButton btnMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producto);
+        btnMenu=findViewById(R.id.btn_atras);
+        btnMenu.setOnClickListener(v->{
+            startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+        });
+
     }
 }
